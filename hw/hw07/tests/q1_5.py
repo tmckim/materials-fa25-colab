08438bfe7d2013bf235a57c8f8416a67227dd126
tmckim <thm5013@gmail.com>
2025-10-20 07:55:49 -0700

    add lec14 demo nb and hw07 nb

---
+++ hw/hw07/tests/q1_5.py
@@ -0,0 +1,10 @@
+OK_FORMAT = True
+
+test = {   'name': 'q1_5',
+    'points': [5, 5],
+    'suites': [   {   'cases': [   {'code': '>>> is_big_win(final_scores.row(2)) is True\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> import numpy as np\n>>> type(big_wins) == np.ndarray\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
